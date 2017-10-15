1f261580d4c7bf66d43a7f0d0f8812e8d84f93cf
AedesAejyptis <anjoslucas7@gmail.com>
2017-10-15 09:55:50 -0400

    telas Mainn Entrar e Cadastre-se  estreuturadas

--- Projeto/Economize/app/src/main/java/com/example/aedes/economize/MainActivity.java
+++ Projeto/Economize/app/src/main/java/com/example/aedes/economize/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.aedes.economize;
 
-import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 
-    public void voltar(View v){
-        Intent intent = new Intent(this, MainActivity.class);
+    public void entrar(View v){
+        Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
     }
+
+    public void singUp(View v){
+        Intent intent = new Intent(this,SingUpActivity.class);
+        startActivity(intent);
+    }
+
 }
--- Projeto/Economize/app/src/main/java/com/example/aedes/economize/SingUpActivity.java
+++ Projeto/Economize/app/src/main/java/com/example/aedes/economize/SingUpActivity.java
@@ -1,21 +1,20 @@
 package com.example.aedes.economize;
 
-import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class SingUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sing_up);
     }
 
     public void voltar(View v){
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
 }
