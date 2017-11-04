8fae2ef9629a1fd2cb1ac94d0c7a5d0f99a9dae1
AedesAejyptis <anjoslucas7@gmail.com>
2017-11-03 22:40:24 -0400

    tela add categoria

--- Projeto/Economize/app/src/main/java/com/example/aedes/economize/T_recorrente.java
+++ Projeto/Economize/app/src/main/java/com/example/aedes/economize/T_recorrente.java
@@ -7,6 +7,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -22,6 +24,9 @@ public class T_recorrente extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Spinner spnnOcorrencia;
+    private String [] valOcorrencias =
+            {"Diaria","Semanal","Quinzenal","Mensal","Bimestral","Trimestral","Semestral","Anual"};
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,7 +69,12 @@ public class T_recorrente extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_t_recorrente, container, false);
+        View v = inflater.inflate(R.layout.fragment_t_recorrente, container, false);
+        spnnOcorrencia = (Spinner)v.findViewById(R.id.spnn_ocorrencia);
+        ArrayAdapter<String> listaOcorrencias =  new ArrayAdapter<String>(this.getActivity(),R.layout.fragment_t_recorrente,valOcorrencias);
+        listaOcorrencias.setDropDownViewResource(R.layout.fragment_t_recorrente);
+        spnnOcorrencia.setAdapter(listaOcorrencias);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
