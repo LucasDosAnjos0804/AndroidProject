0269415a6247851e690aa800ebe768339cc02c84
AedesAejyptis <anjoslucas7@gmail.com>
2017-11-24 20:01:23 -0400

    telas - gráficos de perdas- e - gráficos de ganhos-

---
+++ Projeto/Economize/app/src/main/java/com/example/aedes/economize/frags_graficos/FragGrafGanho.java
@@ -0,0 +1,30 @@
+package com.example.aedes.economize.frags_graficos;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aedes.economize.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragGrafGanho extends Fragment {
+
+
+    public FragGrafGanho() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_graf_ganho, container, false);
+    }
+
+}
---
+++ Projeto/Economize/app/src/main/java/com/example/aedes/economize/frags_graficos/FragGrafPerda.java
@@ -0,0 +1,30 @@
+package com.example.aedes.economize.frags_graficos;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aedes.economize.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragGrafPerda extends Fragment {
+
+
+    public FragGrafPerda() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_graf_perda, container, false);
+    }
+
+}
